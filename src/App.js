6a7246760eee6d0f33f4d026dbe73db79bcaa0e4
marouane <dhambrimarouane@gmail.com>
2021-04-25 01:06:05 +0200

    changed icon+backgroun

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Element from "./components/Element/Element";
 import Table from "./components/Table/Table";
+import image from "./chem.png";
 
 function App() {
   return (
     <div className="App">
-      {/* {elements ? <Table elements={elements} /> : "spinner"} */}
+      <img className="backImage" src={image} />
       <Switch>
         <Route exact path="/" component={Table} />
         <Route path="/:id" component={Element} />
